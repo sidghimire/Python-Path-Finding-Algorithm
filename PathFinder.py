194ac5925993b3607336388cff7711ae482677b4
Sid Ghimire <siddharthaghimire@gmail.com>
2022-09-03 23:51:25 +0545

    First Version Completed

--- PathFinder.py
+++ PathFinder.py
@@ -1,4 +1,6 @@
+from typing import final
 import pygame,sys,math
+import numpy as np
 
 # Initializing Pygame
 pygame.init()
@@ -16,14 +18,16 @@ blue=(0,0,255)
 surface.fill(white)
 
 
-board=[[1 for col in range(8)] for row in range(8)]
-a,b=6,2
-p,q=1,6
-board[a][b]=2
-board[p][q]=3
+board=np.ones((8,8),dtype=int)
+start_y,start_x=1,6
+end_y,end_x=7,0
+board[start_x][start_y]=2
+board[end_x][end_y]=3
 logic=board
 found=False
-
+solving=True
+lowestTrade=None
+finalTraced=[[start_x,start_y]]
 def drawBoxes(board):
     for i in range(len(board)):
         for j in range(len(board[i])):
@@ -44,58 +48,79 @@ def drawButton():
     pygame.draw.rect(surface,red,pygame.Rect(100,300,100,50))
     pygame.display.flip()
 
-def getPoint(a,b,a1,a2):
-    ans=math.pow(a-a1,2)+math.pow(b-a2,2)
-    ans=round(math.pow(ans,0.5),1)
-    ans2=math.pow(p-a1,2)+math.pow(q-a2,2)
-    ans2=round(math.pow(ans,0.5),1)
-    sum=ans*10+ans2*10
-    logic[a1][a2]=sum
-    return sum
-def calculateScore(a,b):
-    val=[0]
-    
-    a1=a+1
-    a2=b
-    sum=getPoint(a,b,a1,a2)
-    val.append([a1,a2,sum])
-    a1=a-1
-    a2=b
-    sum=getPoint(a,b,a1,a2)
-    val.append([a1,a2,sum])
-    a1=a
-    a2=b+1
-    sum=getPoint(a,b,a1,a2)
-    val.append([a1,a2,sum])
-    a1=a
-    a2=b-1
-    sum=getPoint(a,b,a1,a2)
-    val.append([a1,a2,sum])
-    a1=a+1
-    a2=b+1
-    sum=getPoint(a,b,a1,a2)
-    val.append([a1,a2,sum])
-    a1=a+1
-    a2=b-1
-    sum=getPoint(a,b,a1,a2)
-    val.append([a1,a2,sum])
-    a1=a-1
-    a2=b-1
-    sum=getPoint(a,b,a1,a2)
-    val.append([a1,a2,sum])
-    a1=a-1
-    a2=b+1
-    sum=getPoint(a,b,a1,a2)
-    val.append([a1,a2,sum])
-
-    for k in range(6):
-        low=()
-        score=0
-        
-        print(low)
     
 drawBoxes(board)
 drawButton()
+
+def get_adjacent_points(y,x):
+    arr=[]
+    
+    a1,b1=x+1,y
+    if(a1<8 and a1>=0 and b1<8 and b1>=0):
+        if(board[b1][a1]!=0):
+            arr.append([a1,b1])
+    a1,b1=x-1,y
+    if(a1<8 and a1>=0 and b1<8 and b1>=0):
+        if(board[b1][a1]!=0):
+            arr.append([a1,b1])
+    a1,b1=x,y+1
+    if(a1<8 and a1>=0 and b1<8 and b1>=0):
+        if(board[b1][a1]!=0):
+            arr.append([a1,b1])
+    a1,b1=x,y-1
+    if(a1<8 and a1>=0 and b1<8 and b1>=0):
+        if(board[b1][a1]!=0):
+            arr.append([a1,b1])
+    a1,b1=x+1,y-1
+    if(a1<8 and a1>=0 and b1<8 and b1>=0):
+        if(board[b1][a1]!=0):
+            arr.append([a1,b1])
+    a1,b1=x+1,y+1
+    if(a1<8 and a1>=0 and b1<8 and b1>=0):
+        if(board[b1][a1]!=0):
+            arr.append([a1,b1])
+    a1,b1=x-1,y+1
+    if(a1<8 and a1>=0 and b1<8 and b1>=0):
+        if(board[b1][a1]!=0):
+            arr.append([a1,b1])
+    a1,b1=x-1,y-1
+    if(a1<8 and a1>=0 and b1<8 and b1>=0):
+        if(board[b1][a1]!=0):
+            arr.append([a1,b1])
+    return arr
+
+def getScore(points):
+    length=len(points)
+    arr=[]
+    for i in range(length):
+        point=points[i]
+        x_cord=point[0]
+        y_cord=point[1]
+        x_end=end_y
+        y_end=end_x
+        x_start=start_y
+        y_start=start_x
+        distWithEnd=(math.dist([x_end,y_end],[x_cord,y_cord]))
+        distWithFirst=(math.dist([x_start,y_start],[x_cord,y_cord]))
+        ans=distWithEnd+distWithFirst
+        finalArr=[x_cord,y_cord,ans]
+        arr.append(finalArr)
+    
+    return(arr)
+       
+def findSmallest(score):
+    lowest=[]
+    lowestValue=9999999999999999999999999999999999999
+    for i in range(len(score)):
+        if(score[i][2]<lowestValue):
+            lowest=[]
+            lowest.append([score[i][0],score[i][1]])
+            lowestValue=score[i][2]
+        elif(score[i][2]==lowestValue):
+            lowest.append([score[i][0],score[i][1]])
+    return lowest
+
+
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
@@ -106,10 +131,20 @@ while 1:
                 y=int((ans[1]-10)/35)
                 board[y][x]=0
                 drawBoxes(board)
-    if found==False:
-        test_a,test_b=a,b
-        lowest=calculateScore(test_a,test_b)
-        
-
-    if found==True:
-        print("Found")
+    
+            if ans[0]>100 and ans[0]<200 and ans[1]>300 and ans[1]<350:
+                while solving:
+                    attached=get_adjacent_points(start_x,start_y)
+                    score=getScore(attached)
+                    smallest=findSmallest(score)
+                    print(len(smallest))
+                    start_x,start_y=smallest[0][1],smallest[0][0]
+                    finalTraced.append([start_x,start_y])
+                    if([end_y,end_x] in smallest):
+                        print("Path Found")
+                        for i in range(len(finalTraced)):
+                            table=finalTraced[i]
+                            board[table[0]][table[1]]=4
+                            drawBoxes(board)
+                            solving=False
+                        break
